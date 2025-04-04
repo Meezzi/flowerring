3ae63cc81d422ba401453a11ff462530cb17dac5
MoonHo <75512752+monho@users.noreply.github.com>
2025-04-04 11:56:43 +0900

    fix : 주석 제거 및 자동 줄 바꿈

--- lib/pages/review/widgets/review_modal_widgets.dart
+++ lib/pages/review/widgets/review_modal_widgets.dart
@@ -26,7 +26,7 @@ Widget buildStarRating(int currentRating, ValueChanged<int> onChanged) {
 }
 
 /// 리뷰 입력창
-Widget buildReviewTextField(TextEditingController controller){
+Widget buildReviewTextField(TextEditingController controller) {
   return SizedBox(
     height: 500,
     child: TextField(
@@ -35,10 +35,10 @@ Widget buildReviewTextField(TextEditingController controller){
       expands: true,
       decoration: const InputDecoration(
         hintText: "리뷰 내용을 입력하세요.",
-        border : OutlineInputBorder(),
-        contentPadding: EdgeInsets.only(top : 12, left: 12),
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.only(top: 12, left: 12),
       ),
-    )
+    ),
   );
 }
 
@@ -50,12 +50,10 @@ Widget buildSubmitButton({
   return ElevatedButton(
     onPressed: onPressed,
     style: ElevatedButton.styleFrom(
-      minimumSize: const Size.fromHeight(55), // 높이 55
-      backgroundColor: const Color(0xFFFF7676), // 배경색
-      foregroundColor: Colors.white, // 텍스트 색
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20), // 둥근 테두리
-      ),
+      minimumSize: const Size.fromHeight(55),
+      backgroundColor: const Color(0xFFFF7676),
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
     ),
     child: Text(label),
   );
