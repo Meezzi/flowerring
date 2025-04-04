17cdc78f0806d8086d95336b6414672ff956acb1
Meezzi <alswl-3016@naver.com>
2025-04-04 10:50:57 +0900

    feat: AppBar title 설정

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CartPage extends StatefulWidget{
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
   @override
@@ -10,6 +10,13 @@ class CartPage extends StatefulWidget{
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        // iOS에서는 기본으로 AppBar title이 가운데 정렬되지만,
+        // Android에서는 title이 왼쪽 정렬되기 때문에 centerTitle을 true로 설정
+        centerTitle: true,
+        title: Text('장바구니'),
+      ),
+    );
   }
 }
