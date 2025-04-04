01f6f369035e0a7e9d1ce46f3aaae4dea1f8669f
MoonHo <75512752+monho@users.noreply.github.com>
2025-04-04 12:49:31 +0900

    fix : 줄바꿈 정리

--- lib/pages/review/review_modal_controller.dart
+++ lib/pages/review/review_modal_controller.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import './review_page.dart';
 
-void showReviewModal(BuildContext context, VoidCallback onClose, ValueChanged<int> onSubmit) {
+void showReviewModal(
+  BuildContext context,
+  VoidCallback onClose,
+  ValueChanged<int> onSubmit,
+) {
   showDialog(
     context: context,
-    builder: (_) => ReviewModal(
-      onClose: onClose,
-      onSubmit: onSubmit,
-    ),
+    builder: (_) => ReviewModal(onClose: onClose, onSubmit: onSubmit),
   );
 }
