d802a69c3cb7e9df7307ca5b7b0a9a3581386fd0
Meezzi <alswl-3016@naver.com>
2025-04-06 23:41:43 +0900

    design: 장바구니 페이지 배경 색상을 하얀색으로 설정

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -11,6 +11,7 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         // iOS에서는 기본으로 AppBar title이 가운데 정렬되지만,
         // Android에서는 title이 왼쪽 정렬되기 때문에 centerTitle을 true로 설정
