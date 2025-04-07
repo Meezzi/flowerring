ba90c927fb1dc7cf1d173d6982c4ee4782ef4503
Meezzi <alswl-3016@naver.com>
2025-04-07 12:47:55 +0900

    feat: 상품의 리뷰 숫자를 저장하는 변수 추가

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -4,6 +4,7 @@ class Product {
   final String content;
   final int price;
   final double rate;
+  final int reviewsCount;
 
   // 생성자
   Product({
@@ -12,6 +13,7 @@ class Product {
     required this.content,
     required this.price,
     required this.rate,
+    required this.reviewsCount,
   });
 
   // 상품 데이터를 반환하는 메소드
@@ -23,6 +25,7 @@ class Product {
         content: '맛있는 사과입니다.',
         price: 10000,
         rate: 4.5,
+        reviewsCount: 0,
       ),
       Product(
         imageUrl: 'assets/sample.jpg',
@@ -30,6 +33,7 @@ class Product {
         content: '맛있는 바나나입니다..',
         price: 10000,
         rate: 4.5,
+        reviewsCount: 0,
       ),
       Product(
         imageUrl: 'assets/sample.jpg',
@@ -37,6 +41,7 @@ class Product {
         content: '맛있는 오렌지입니다.',
         price: 10000,
         rate: 4.5,
+        reviewsCount: 0,
       ),
     ];
   }
