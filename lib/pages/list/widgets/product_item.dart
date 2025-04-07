3689c65ec9740233ab2cf8d13f4c216ecadd05fe
dlworua <leegaegyum@gmail.com>
2025-04-07 18:17:35 +0900

    feat: 상품 목록 위젯의 가격과 상품명 폰트 굵게하고 Image.network를 Image.asset으로 수정 후 이미지 둥글게 구현

--- lib/pages/list/widgets/product_item.dart
+++ lib/pages/list/widgets/product_item.dart
@@ -9,19 +9,20 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       width: double.infinity,
-      height: 80,
+      height: 100,
       decoration: BoxDecoration(
         color: Colors.transparent,
         border: Border(bottom: BorderSide(color: Colors.grey[300]!)),
       ),
       child: Row(
         children: [
-          ClipRect(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
             child: AspectRatio(
               aspectRatio: 1,
-              child: Image.network(product.imageUrl, fit: BoxFit.cover),
+              child: Image.asset(product.imageUrl, fit: BoxFit.cover),
             ),
           ),
           const SizedBox(width: 10),
@@ -30,14 +31,22 @@ class ProductItem extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(product.title),
+                Text(
+                  product.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
                 Row(
                   children: [
                     Icon(Icons.star, color: Colors.yellow),
                     Text('${product.rate}'),
-                    const SizedBox(height: 45),
-                    const SizedBox(width: 150),
-                    Text('${product.price}원'),
+                    const SizedBox(height: 50, width: 150),
+                    Text(
+                      '${product.price}원',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ],
