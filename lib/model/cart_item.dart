6beafdb6b43b294375b98e764bbd0b8fd2f5001e
Meezzi <alswl-3016@naver.com>
2025-04-07 16:38:09 +0900

    feat: 장바구니에 담긴 상품을 관리하는 CartItem 클래스 생성

---
+++ lib/model/cart_item.dart
@@ -0,0 +1,8 @@
+import 'package:flowerring/model/product.dart';
+
+class CartItem {
+  final Product product;
+  final int quantity;
+
+  CartItem({required this.product, required this.quantity});
+}
