2caa5515eed82308e9f0868bca9d8147d4439d33
Meezzi <alswl-3016@naver.com>
2025-04-07 12:45:48 +0900

    feat: Product class에 상품 상세 설명 추가

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
-
 class Product {
   final String imageUrl;
   final String title;
+  final String content;
   final int price;
   final double rate;
 
@@ -10,6 +9,7 @@ class Product {
   Product({
     required this.imageUrl,
     required this.title,
+    required this.content,
     required this.price,
     required this.rate,
   });
@@ -20,18 +20,21 @@ class Product {
       Product(
         imageUrl: 'assets/sample.jpg',
         title: '사과',
+        content: '맛있는 사과입니다.',
         price: 10000,
         rate: 4.5,
       ),
       Product(
         imageUrl: 'assets/sample.jpg',
         title: '바나나',
+        content: '맛있는 바나나입니다..',
         price: 10000,
         rate: 4.5,
       ),
       Product(
         imageUrl: 'assets/sample.jpg',
         title: '오렌지',
+        content: '맛있는 오렌지입니다.',
         price: 10000,
         rate: 4.5,
       ),
