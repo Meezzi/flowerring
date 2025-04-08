53bdfa44c067b97bb0f3d2980f90c2a2b9437d9a
Meezzi <alswl-3016@naver.com>
2025-04-08 12:10:41 +0900

    feat: 상품 재고 변경

--- lib/pages/list/list_page.dart
+++ lib/pages/list/list_page.dart
@@ -1,3 +1,4 @@
+import 'package:flowerring/model/cart_item.dart';
 import 'package:flowerring/model/product.dart';
 import 'package:flowerring/pages/cart/cart_page.dart';
 import 'package:flowerring/pages/detail/detail_page.dart';
@@ -13,6 +14,24 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+List<Product> _items = [];
+
+  void checkout(List<CartItem> itemsInCart) {
+    for (var cartItem in itemsInCart) {
+      // 장바구니의 상품 ID와 일치하는 상품 찾기
+      final productIndex = _items.indexWhere(
+        (product) => product.id == cartItem.product.id,
+      );
+
+      // 상품의 재고에서 선택한 수량만큼 감소
+      if (productIndex != -1) {
+        setState(() {
+          _items[productIndex].stock -= cartItem.quantity;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // Product.getProducts()로 상품 데이터 가져오기
