78d118cec5c1adaf0da8dd845c22df6110c00642
Meezzi <alswl-3016@naver.com>
2025-04-08 16:35:13 +0900

    design: ElevatedButton 스타일 설정

--- lib/theme/dark.dart
+++ lib/theme/dark.dart
@@ -5,4 +5,15 @@ final ThemeData darkTheme = ThemeData(
   colorScheme: ColorScheme.dark(
     primary: Color.fromARGB(255, 255, 118, 118),
   ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 255, 118, 118),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+  ),
 );
