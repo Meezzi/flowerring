2c11c0f3a0caaa1d20c36ec33f5c3d278388e552
Meezzi <alswl-3016@naver.com>
2025-04-08 15:53:00 +0900

    design: dartk theme 메인 색상 설정

---
+++ lib/theme/dark.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    primary: Color.fromARGB(255, 255, 118, 118),
+  ),
+);
